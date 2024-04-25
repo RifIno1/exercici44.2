c4538e0f82fb11ceeb4b47c8c88a325cbd77f9f1
Othman Git <othman.douiri1@gmail.com>
2024-04-25 20:40:01 +0200

    class joc creada

--- src/JOC.java
+++ src/JOC.java
@@ -1,2 +1,28 @@
+import jdk.jshell.spi.ExecutionControl;
+
 public class JOC {
+private char[][] taulell;
+private short torn;
+
+    public char[][] getTaulell() {
+        return taulell;
+    }
+
+    public short getTorn() {
+        return torn;
+    }
+
+    public static void novaPartida(){
+        throw new UnsupportedOperationException("Not implemented");
+    }
+
+    public static void jugar(short fila,String columna){
+        throw new UnsupportedOperationException("Not implemented");
+    }
+
+    public static boolean jugadaGuanyadora(short fila,String columna){
+        throw new UnsupportedOperationException("Not implemented");
+    }
+
+
 }
--- src/TUI.java
+++ src/TUI.java
@@ -1,2 +1,28 @@
+import java.util.Scanner;
+
 public class TUI {
+
+private Scanner sc = new Scanner(System.in);
+
+public static int mostrarMenu(){
+    throw new UnsupportedOperationException("Not implemented");
+}
+
+    public static int mostrarTaulell(char[][] taulell,short torn){
+        throw new UnsupportedOperationException("Not implemented");
+    }
+
+    public static short[] recollirJugada(){
+        throw new UnsupportedOperationException("Not implemented");
+    }
+
+    public static void fiDePartida(short guanyador){
+        throw new UnsupportedOperationException("Not implemented");
+
+    }
+
+
+
+
+
 }
