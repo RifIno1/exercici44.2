833337298d1f65c906d07bbe9482f92fc30d490d
AchrafIbrahimi <achrafibrahimi.1998@gmail.com>
2024-05-02 20:05:57 +0200

    adjust classes

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,41 @@ public class Main {
 
     public static void main(String[] args) {
 
+        TUI tui = new TUI();
+        JOC joc = new JOC();
 
-
+        int resultado = tui.mostrarMenu();
+        System.out.print(resultado);
+        switch (resultado){
+            case 1:
+                nuevapartida();
+                break;
+            case 2:
+                cargapartida();
+                break;
+            case 3:
+                configuracion();
+                break;
+            case 4:
+                salir();
+                break;
+        }
+    
+    
     }
 
+    public static void nuevapartida() {
+    }
+
+    public static void cargapartida() {
+    }
+
+    public static void configuracion() {
+    }
+
+    public static void salir() {
+    }
+
+    
+    
 }
--- src/TUI.java
+++ src/TUI.java
@@ -1,2 +1,11 @@
 public class TUI {
+
+
+    public int mostrarMenu() {
+
+    }
+
+
+
+
 }
