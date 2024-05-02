c3505696e4a327ecf05a577234b88753f97c1f42
AchrafIbrahimi <achrafibrahimi.1998@gmail.com>
2024-05-02 20:22:52 +0200

    adjust Tui

--- src/TUI.java
+++ src/TUI.java
@@ -1,12 +1,19 @@
-public class TUI {
+import java.util.Scanner;
 
+class Tui {
+    Scanner scanner;
 
-    public int mostrarMenu() {
-
-        return 0;
+    public Tui() {
+        this.scanner = new Scanner(System.in);
     }
 
-
-
+    public int mostrarMenu() {
+        System.out.println("1. Nova partida");
+        System.out.println("2. Carregar partida");
+        System.out.println("3. Configuració");
+        System.out.println("4. Sortir");
+        System.out.print("Escull una opció: ");
+        return scanner.nextInt();
+    }
 
 }
