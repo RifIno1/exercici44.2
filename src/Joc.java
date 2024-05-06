d9d61611f1877b73b1a9372921ee913bd8b1a35c
AchrafIbrahimi <achrafibrahimi.1998@gmail.com>
2024-05-06 19:44:58 +0200

    function check winner

--- src/Joc.java
+++ src/Joc.java
@@ -6,11 +6,37 @@ class Joc {
     }
 
     public static void novaPartida() {
+        char torn = 'X';
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 taulell[i][j] = ' ';
             }
         }
+    }
+
+    // check if the game is over
+    public static boolean comprovarGuanyador(char torn) {
+        // check rows
+        for (int i = 0; i < 3; i++) {
+            if (taulell[i][0] == torn && taulell[i][1] == torn && taulell[i][2] == torn) {
+                return true;
+            }
+        }
+        // check columns
+        for (int i = 0; i < 3; i++) {
+            if (taulell[0][i] == torn && taulell[1][i] == torn && taulell[2][i] == torn) {
+                return true;
+            }
+        }
+        // check diagonals
+        if (taulell[0][0] == torn && taulell[1][1] == torn && taulell[2][2] == torn) {
+            return true;
+        }
+        if (taulell[0][2] == torn && taulell[1][1] == torn && taulell[2][0] == torn) {
+            return true;
+        }
+        return false;
+        
     }
 
     /* 
